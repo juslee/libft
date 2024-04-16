fb44022516c33fe96d1cd478057cd8e655f5bcad
Justin Lee <triplez@justinlee.sg>
2024-04-16 16:46:21 +0800

    add ft_strlen and ft_memcpy

--- includes/ft_string.h
+++ includes/ft_string.h
@@ -6,7 +6,7 @@
 /*   By: welee <welee@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 17:16:33 by welee             #+#    #+#             */
-/*   Updated: 2024/04/16 16:25:50 by welee            ###   ########.fr       */
+/*   Updated: 2024/04/16 16:44:03 by welee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 
 #endif
--- srcs/ft_string/ft_strlen.c
+++ srcs/ft_string/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: welee <welee@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 17:13:44 by welee             #+#    #+#             */
-/*   Updated: 2024/04/12 17:16:06 by welee            ###   ########.fr       */
+/*   Updated: 2024/04/16 16:40:53 by welee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 /// @brief Calculate the length of a string
 /// @param s The string to calculate the length
 /// @return The length of the string
-size_t	strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
 	size_t	len;
 
