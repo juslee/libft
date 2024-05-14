74ce462d20ced485eaba3cc9f95c80642121dc2f
Justin Lee <triplez@justinlee.sg>
2024-05-14 14:03:06 +0800

    change ft_ispunct

--- tests/ft_ctype/ft_ispunct/ft_ispunct.c
+++ tests/ft_ctype/ft_ispunct/ft_ispunct.c
@@ -6,7 +6,7 @@
 /*   By: welee <welee@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 15:26:56 by welee             #+#    #+#             */
-/*   Updated: 2024/05/02 15:28:09 by welee            ###   ########.fr       */
+/*   Updated: 2024/05/14 14:02:44 by welee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,25 @@ void	test_ispunct_punctuation(void)
 	c = 33;
 	while (c < 48)
 	{
-		assert(ft_ispunct(c) == ispunct(c));
+		assert(ft_ispunct(c) > 0 && ispunct(c) > 0);
 		++c;
 	}
 	c = 58;
 	while (c < 65)
 	{
-		assert(ft_ispunct(c) == ispunct(c));
+		assert(ft_ispunct(c) > 0 && ispunct(c) > 0);
 		++c;
 	}
 	c = 91;
 	while (c < 97)
 	{
-		assert(ft_ispunct(c) == ispunct(c));
+		assert(ft_ispunct(c) > 0 && ispunct(c) > 0);
 		++c;
 	}
 	c = 123;
 	while (c < 127)
 	{
-		assert(ft_ispunct(c) == ispunct(c));
+		assert(ft_ispunct(c) > 0 && ispunct(c) > 0);
 		++c;
 	}
 	printf("test_ispunct_punctuation passed.\n");
@@ -53,31 +53,31 @@ void	test_ispunct_non_punctuation(void)
 	c = 0;
 	while (c < 33)
 	{
-		assert(ft_ispunct(c) == ispunct(c));
+		assert(ft_ispunct(c) == 0 && ispunct(c) == 0);
 		++c;
 	}
 	c = 48;
 	while (c < 58)
 	{
-		assert(ft_ispunct(c) == ispunct(c));
+		assert(ft_ispunct(c) == 0 && ispunct(c) == 0);
 		++c;
 	}
 	c = 65;
 	while (c < 91)
 	{
-		assert(ft_ispunct(c) == ispunct(c));
+		assert(ft_ispunct(c) == 0 && ispunct(c) == 0);
 		++c;
 	}
 	c = 97;
 	while (c < 123)
 	{
-		assert(ft_ispunct(c) == ispunct(c));
+		assert(ft_ispunct(c) == 0 && ispunct(c) == 0);
 		++c;
 	}
 	c = 127;
 	while (c < 256)
 	{
-		assert(ft_ispunct(c) == ispunct(c));
+		assert(ft_ispunct(c) == 0 && ispunct(c) == 0);
 		++c;
 	}
 	printf("test_ispunct_non_punctuation passed.\n");
@@ -90,7 +90,7 @@ void	test_ispunct_negative(void)
 	c = -128;
 	while (c < 0)
 	{
-		assert(ft_ispunct(c) == ispunct(c));
+		assert(ft_ispunct(c) == 0 && ispunct(c) == 0);
 		++c;
 	}
 	printf("test_ispunct_negative passed.\n");
