8a164ed4140fe3a8ed570c44737b8ae4a355b249
Justin Lee <triplez@justinlee.sg>
2024-05-14 14:08:54 +0800

    change ft_strlcat

--- tests/ft_string/ft_strlcat/ft_strlcat.c
+++ tests/ft_string/ft_strlcat/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: welee <welee@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 11:34:19 by welee             #+#    #+#             */
-/*   Updated: 2024/04/26 20:49:51 by welee            ###   ########.fr       */
+/*   Updated: 2024/05/14 14:08:25 by welee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ void	test_strlcat_basic(void)
 	const char		*src = ", world!";
 	const size_t	result = ft_strlcat(dst, src, sizeof(dst));
 
-	assert(strcmp(dst, "Hello, world!") == 0);
-	assert(result == strlen("Hello, world!"));
+	assert(ft_strcmp(dst, "Hello, world!") == 0);
+	assert(result == ft_strlen("Hello, world!"));
 	printf("test_strlcat_basic passed.\n");
 }
 
@@ -32,8 +32,8 @@ void	test_strlcat_buffer_too_small(void)
 	const char		*src = ", world!";
 	const size_t	result = ft_strlcat(dst, src, sizeof(dst));
 
-	assert(strcmp(dst, "Hello, wor") == 0);
-	assert(result == strlen("Hello") + strlen(src));
+	assert(ft_strcmp(dst, "Hello, wor") == 0);
+	assert(result == ft_strlen("Hello") + ft_strlen(src));
 	printf("test_strlcat_buffer_too_small passed.\n");
 }
 
@@ -43,7 +43,7 @@ void	test_strlcat_exact_fit(void)
 	const char		*src = ", world!";
 	const size_t	result = ft_strlcat(dst, src, sizeof(dst));
 
-	assert(strcmp(dst, "Hello, worl") == 0);
+	assert(ft_strcmp(dst, "Hello, worl") == 0);
 	assert(result == 13);
 	printf("test_strlcat_exact_fit passed.\n");
 }
@@ -52,10 +52,10 @@ void	test_strlcat_zero_length(void)
 {
 	char			dst[10] = "Hello";
 	const char		*src = "";
-	const size_t	result = strlcat(dst, src, sizeof(dst));
+	const size_t	result = ft_strlcat(dst, src, sizeof(dst));
 
-	assert(strcmp(dst, "Hello") == 0);
-	assert(result == strlen("Hello"));
+	assert(ft_strcmp(dst, "Hello") == 0);
+	assert(result == ft_strlen("Hello"));
 	printf("test_strlcat_zero_length passed.\n");
 }
 
