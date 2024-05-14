8f155c154c031caef6737cd8ee7524dddb83e23c
Justin Lee <triplez@justinlee.sg>
2024-05-14 14:45:54 +0800

    change ft_strnstr tests

--- tests/ft_string/ft_strnstr/ft_strnstr.c
+++ tests/ft_string/ft_strnstr/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: welee <welee@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 14:29:32 by welee             #+#    #+#             */
-/*   Updated: 2024/04/26 21:33:53 by welee            ###   ########.fr       */
+/*   Updated: 2024/05/14 14:44:27 by welee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,79 +15,72 @@
 #include <assert.h>
 #include "libft.h"
 
-void	test_strnstr_basic(void)
+void	test_found_within_length(void)
 {
-	const char	*big = "Hello, world!";
-	const char	*little = "world";
-	const char	*result = ft_strnstr(big, little, ft_strlen(big));
-	const char	*expected = strnstr(big, little, ft_strlen(big));
+	const char	*result = ft_strnstr("Hello, world!", "world", 13);
 
-	assert(result == expected);
-	printf("test_strnstr_basic passed.\n");
+	assert(result && ft_strcmp(result, "world!") == 0);
 }
 
-void	test_strnstr_not_found(void)
+void	test_not_found_within_length(void)
 {
-	const char	*big = "Hello, world!";
-	const char	*little = "world!";
-	const char	*result = ft_strnstr(big, little, 6);
-
-	assert(result == NULL);
-	printf("test_strnstr_not_found passed.\n");
+	assert(ft_strnstr("Hello, world!", "world", 7) == NULL);
 }
 
-void	test_strnstr_empty_needle(void)
+void	test_needle_at_end(void)
 {
-	const char	*big = "Hello, world!";
-	const char	*little = "";
-	const char	*result = ft_strnstr(big, little, ft_strlen(big));
-	const char	*expected = strnstr(big, little, ft_strlen(big));
+	const char	*result = ft_strnstr("Hello, world!", "world!", 13);
 
-	assert(result == expected);
-	printf("test_strnstr_empty_needle passed.\n");
+	assert(result && ft_strcmp(result, "world!") == 0);
 }
 
-void	test_strnstr_empty_haystack(void)
+void	test_needle_beyond_length(void)
 {
-	const char	*big = "";
-	const char	*little = "world";
-	const char	*result = ft_strnstr(big, little, ft_strlen(big));
-	const char	*expected = strnstr(big, little, ft_strlen(big));
-
-	assert(result == expected);
-	printf("test_strnstr_empty_haystack passed.\n");
+	assert(ft_strnstr("Hello, world!", "world", 11) == NULL);
 }
 
-void	test_strnstr_empty_both(void)
+void	test_needle_longer_than_haystack(void)
 {
-	const char	*big = "";
-	const char	*little = "";
-	const char	*result = ft_strnstr(big, little, ft_strlen(big));
-	const char	*expected = strnstr(big, little, ft_strlen(big));
-
-	assert(result == expected);
-	printf("test_strnstr_empty_both passed.\n");
+	assert(ft_strnstr("Hello", "Hello, world!", 5) == NULL);
 }
 
-void	test_strnstr_zero_length(void)
+void	test_empty_needle(void)
 {
-	const char	*big = "Hello, world!";
-	const char	*little = "world";
-	const char	*result = ft_strnstr(big, little, 0);
-	const char	*expected = strnstr(big, little, 0);
+	const char	*result = ft_strnstr("Hello, world!", "", 13);
 
-	assert(result == expected);
-	printf("test_strnstr_zero_length passed.\n");
+	assert(result && ft_strcmp(result, "Hello, world!") == 0);
+}
+
+void	test_empty_haystack(void)
+{
+	assert(ft_strnstr("", "world", 0) == NULL);
+}
+
+void	test_both_empty(void)
+{
+	const char	*result = ft_strnstr("", "", 0);
+
+	assert(result && ft_strcmp(result, "") == 0);
+}
+
+void	test_same_haystack_and_needle(void)
+{
+	const char	*result = ft_strnstr("Hello", "Hello", 5);
+
+	assert(result && ft_strcmp(result, "Hello") == 0);
 }
 
 int	main(void)
 {
-	test_strnstr_basic();
-	test_strnstr_not_found();
-	test_strnstr_empty_needle();
-	test_strnstr_empty_haystack();
-	test_strnstr_empty_both();
-	test_strnstr_zero_length();
+	test_found_within_length();
+	test_not_found_within_length();
+	test_needle_at_end();
+	test_needle_beyond_length();
+	test_needle_longer_than_haystack();
+	test_empty_needle();
+	test_empty_haystack();
+	test_both_empty();
+	test_same_haystack_and_needle();
 
 	printf("All tests passed.\n");
 	return (0);
