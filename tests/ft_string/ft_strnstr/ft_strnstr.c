6d660846ec531c9e05539f8fa59363a7f1c4d70b
Justin Lee <triplez@justinlee.sg>
2024-05-19 09:45:32 +0800

    change ft_strnstr to remove strnstr

--- tests/ft_string/ft_strnstr/ft_strnstr.c
+++ tests/ft_string/ft_strnstr/ft_strnstr.c
@@ -6,13 +6,14 @@
 /*   By: welee <welee@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 14:29:32 by welee             #+#    #+#             */
-/*   Updated: 2024/05/17 15:39:20 by welee            ###   ########.fr       */
+/*   Updated: 2024/05/19 09:44:53 by welee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+// #include <bsd/string.h>
 #include "libft.h"
 
 void	test_found_within_length(void)
@@ -70,16 +71,16 @@ void	test_same_haystack_and_needle(void)
 	assert(result && strcmp(result, "Hello") == 0);
 }
 
-void	test(void)
-{
-	const char	*result = ft_strnstr("Hello", "Hel", 2);
-	const char	*result2 = strnstr("Hello", "Hel", 2);
+// void	test(void)
+// {
+// 	const char	*result = ft_strnstr("Hello", "Hel", 2);
+// 	const char	*result2 = strnstr("Hello", "Hel", 2);
 
-	printf("ft_strnstr: %s\n", result);
-	printf("strnstr: %s\n", result2);
-	assert(result == result2);
-	printf("Test passed.\n");
-}
+// 	printf("ft_strnstr: %s\n", result);
+// 	printf("strnstr: %s\n", result2);
+// 	assert(result == result2);
+// 	printf("Test passed.\n");
+// }
 
 int	main(void)
 {
@@ -92,7 +93,6 @@ int	main(void)
 	test_empty_haystack();
 	test_both_empty();
 	test_same_haystack_and_needle();
-	test();
 
 	printf("All tests passed.\n");
 	return (0);
