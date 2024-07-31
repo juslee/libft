a9a1212b9c6dffe36329a6b2ed2bdd66eccd12b4
Justin Lee <triplez@justinlee.sg>
2024-07-31 18:20:15 +0800

    Refactor ft_math.h and implement ft_max and ft_min functions

--- includes/ft_math.h
+++ includes/ft_math.h
@@ -1,28 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_math.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: welee <welee@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/11 16:35:11 by welee             #+#    #+#             */
-/*   Updated: 2024/07/31 15:21:17 by welee            ###   ########.fr       */
+/*   Created: 2024/07/31 15:21:07 by welee             #+#    #+#             */
+/*   Updated: 2024/07/31 15:21:41 by welee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include "ft_btree.h"
-# include "ft_ctype.h"
-# include "ft_ctype_extra.h"
-# include "ft_func.h"
-# include "ft_list.h"
-# include "ft_queue.h"
-# include "ft_math.h"
-# include "ft_sort.h"
-# include "ft_stdlib.h"
-# include "ft_string.h"
-# include "ft_string_extra.h"
-# include "ft_unistd.h"
-# include "ft_unistd_extra.h"
+#ifndef FT_MATH_H
+# define FT_MATH_H
+
+int	ft_max(int a, int b);
+int	ft_min(int a, int b);
+
 #endif
--- srcs/ft_math/ft_max.c
+++ srcs/ft_math/ft_max.c
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_max.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: welee <welee@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/11 16:35:11 by welee             #+#    #+#             */
-/*   Updated: 2024/07/31 15:21:17 by welee            ###   ########.fr       */
+/*   Created: 2024/07/31 15:18:44 by welee             #+#    #+#             */
+/*   Updated: 2024/07/31 15:19:36 by welee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include "ft_btree.h"
-# include "ft_ctype.h"
-# include "ft_ctype_extra.h"
-# include "ft_func.h"
-# include "ft_list.h"
-# include "ft_queue.h"
-# include "ft_math.h"
-# include "ft_sort.h"
-# include "ft_stdlib.h"
-# include "ft_string.h"
-# include "ft_string_extra.h"
-# include "ft_unistd.h"
-# include "ft_unistd_extra.h"
-#endif
+int	ft_max(int a, int b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
